aa2741a7b2a2562f0cc305b03fd33f6bda2a3d62
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-04-01 19:13:56 -0300

    Funcionalidade de fechar menu ao clicar fora - desenvolvida

--- multi/src/components/Header/Nav/Nav.js
+++ multi/src/components/Header/Nav/Nav.js
@@ -5,16 +5,24 @@ import * as S from './Nav.styles';
 const Nav = () => {
   const [mobile, setMobile] = useState(true);
 
+  const handleOutsideClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) setMobile(false);
+  }
+
+  const handleNavLink = () => {
+    setMobile(false);
+  }
+
   return (
     <div>
-      <S.NavContainer mobile={mobile}>
+      <S.NavContainer mobile={mobile} onClick={handleOutsideClick}>
         <S.NavList>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a onClick={handleNavLink} href="#home">Home</a></li>
+          <li><a onClick={handleNavLink} href="#about">About</a></li>
+          <li><a onClick={handleNavLink} href="#services">Services</a></li>
+          <li><a onClick={handleNavLink} href="#portfolio">Portfolio</a></li>
+          <li><a onClick={handleNavLink} href="#team">Team</a></li>
+          <li><a onClick={handleNavLink} href="#contact">Contact</a></li>
         </S.NavList>
 
         <S.ButtonClose onClick={() => setMobile(false)} mobile={mobile}>
