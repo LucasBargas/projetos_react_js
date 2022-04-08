497d143eb5bb587024eacc0b0826d0450788e931
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-04-08 20:25:13 -0300

    Estruturando projeto

--- github_perfil/src/App.js
+++ github_perfil/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './layout/Header/Header';
-import Profile from './pages/Profile';
+import Profile from './pages/Profile/Profile';
 
 const App = () => {
   return (
